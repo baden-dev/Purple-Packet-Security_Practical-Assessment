8efecc3efdf05d473462d74e3c55f039a339ccfc
baden-dev <baden.dev@gmail.com>
2023-07-06 01:05:03 +0200

    Revert to previouse commit	Revert "Added search bar with filers for agent_id, cpu, name and cores. Also added extra colums (ram free and ram total"
    This reverts commit d3b45664c32504a9d100beaa142f6b4438cec2de.

--- client/src/components/HardwarePage.jsx
+++ client/src/components/HardwarePage.jsx
@@ -1,142 +1,89 @@
 import React, { useEffect, useState } from 'react';
 import apis from '../api/index';
 import '../css/Tables.css';
-import '../css/SearchBar.css';
 
-function HardwareTable() {
-  const [hardware, setHardwares] = useState([]);
-  const [searchInput, setSearchInput] = useState('');
-  const [filteredHardware, setFilteredHardware] = useState([]);
-  const [expandedItem, setExpandedItem] = useState(null);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [hardwarePerPage] = useState(5);
+function HardwareTable(){
+    const [hardware, setHardwares] = useState([]);
+    const [expandedItem, setExpandedItem] = useState(null);
 
-  useEffect(() => {
-    const fetchHardwares = async () => {
-      try {
-        const response = await apis.getAllHardware();
-        setHardwares(response.data);
-      } catch (error) {
-        console.error('Error fetching agents:', error);
-      }
+
+    useEffect(() => {
+        const fetchHardwares = async () => {
+        try {
+            const response = await apis.getAllHardware();
+            setHardwares(response.data); 
+        } catch (error) {
+            console.error('Error fetching agents:', error);
+        }
+        };
+
+        fetchHardwares();
+    }, []);
+
+    const handleExpand = (itemId) => {
+        if (expandedItem === itemId) {
+        setExpandedItem(null);
+        } else {
+        setExpandedItem(itemId);
+        }
     };
 
-    fetchHardwares();
-  }, []);
 
-  //search filters
-  useEffect(() => {
-    const filtered = hardware.filter(item => {
-      const agentId = item.agent_id ? item.agent_id.toLowerCase() : '';
-      const cpuName = item.cpu && item.cpu.name ? item.cpu.name.toLowerCase() : '';
-      const cores = item.ram && item.cpu.cores ? item.cpu.cores.toLocaleString() : '';
-      return agentId.includes(searchInput.toLowerCase()) || cpuName.includes(searchInput.toLocaleLowerCase()) || cores.includes(searchInput);
-    });
-
-    setFilteredHardware(filtered);
-    setCurrentPage(1); // Reset to first page when search input changes
-  }, [searchInput, hardware]);
-
-  const handleExpand = (itemId) => {
-    if (expandedItem === itemId) {
-      setExpandedItem(null);
-    } else {
-      setExpandedItem(itemId);
-    }
-  };
-
-  const handleNextPage = () => {
-    setCurrentPage(prevPage => prevPage + 1);
-  };
-
-  const handlePrevPage = () => {
-    setCurrentPage(prevPage => prevPage - 1);
-  };
-
-  const indexOfLastHardware = currentPage * hardwarePerPage;
-  const indexOfFirstHardware = indexOfLastHardware - hardwarePerPage;
-  const currentHardware = filteredHardware.slice(indexOfFirstHardware, indexOfLastHardware);
-
-  const handleSearchInputChange = event => {
-    setSearchInput(event.target.value);
-  };
-
-  return (
-    <div className="table-container">
-      <div className="search-container">
-      {/* Search input */}
-      <input
-        type="text"
-        placeholder="Search by Agent ID, CPU Name or Cores"
-        value={searchInput}
-        onChange={handleSearchInputChange}
-      />
-      {/* Image */}
-      <img src={process.env.PUBLIC_URL + '/images/search_grey.png'} alt="Icon" className="search-icon" />
-      </div>
-      {/* Table */}
-      <table className="table-tag">
-        <thead>
-          <tr>
-            <th>Agent ID</th>
-            <th>CPU Name</th>
-            <th>Cores</th>
-            <th>Ram Free</th>
-            <th>Ram Total</th>
-            <th>Time Scanned</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {currentHardware.map((item) => (
-            < React.Fragment key={item.agent_id} >
+    return (
+        <div className="table-container">
+          <table className="table-tag">
+            <thead>
               <tr>
-                <td>{item.agent_id}</td>
-                <td>{item.cpu.name}</td>
-                <td>{item.cpu.cores}</td>
-                <td>{item.ram.free}</td>
-                <td>{item.ram.total}</td>
-                <td>{formatTime(item.scan.time)}</td>
-                <td>
-                  <button onClick={() => handleExpand(item.agent_id)}>
-                    {expandedItem === item.agent_id ? '-' : '+'}
-                  </button>
-                </td>
+                <th>Agent ID</th>
+                <th>CPU Name</th>
+                <th>Cores</th>
+                <th>Time Scanned</th>
+                <th></th>
               </tr>
-              {expandedItem === item.agent_id && (
+            </thead>
+            <tbody>
+              {hardware.map((item) => (
+                <React.Fragment key={item._id}>
                 <tr>
-                  <td colSpan="5">
-                    <pre>{JSON.stringify(item, null, 2)}</pre>
+                  <td>{item.agent_id}</td>
+                  <td>{item.cpu.name}</td>
+                  <td>{item.cpu.cores}</td>
+                  <td>{formatTime(item.scan.time)}</td>
+                  <td>
+                    <button onClick={() => handleExpand(item._id)}>
+                      {expandedItem === item._id ? '-' : '+'}
+                    </button>
                   </td>
                 </tr>
-              )}
-            </React.Fragment>
-          ))}
-        </tbody>
-      </table>
+                {expandedItem === item._id && (
+                  <tr>
+                    <td colSpan="5">
+                      <pre>{JSON.stringify(item, null, 2)}</pre>
+                    </td>
+                  </tr>
+                )}
+              </React.Fragment>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      );
 
-      {/* Pagination */}
-      <div>
-        <button disabled={currentPage === 1} onClick={handlePrevPage}>Prev</button>
-        <button disabled={currentHardware.length <= hardwarePerPage} onClick={handleNextPage}>Next</button>
-      </div>
-    </div>
-  );
 }
 
 function formatTime(time) {
-  const options = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-    timeZone: 'UTC',
-  };
-
-  const formattedTime = new Date(time).toLocaleString('en-US', options);
-  return formattedTime;
+    const options = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      timeZone: 'UTC',
+    };
+  
+    const formattedTime = new Date(time).toLocaleString('en-US', options);
+    return formattedTime;
 }
 
 export default HardwareTable;
